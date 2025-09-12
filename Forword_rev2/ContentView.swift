9b45c2e792d570b859ed4b026c60991632d739bd
2yum7 <215ibuki@gmail.com>
2025-09-12 15:00:10 +0100

    Initial Commit

---
+++ Forword_rev2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Forword_rev2
+//
+//  Created by Ibuki on 12/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Forword_rev2/Forword_rev2App.swift
@@ -0,0 +1,17 @@
+//
+//  Forword_rev2App.swift
+//  Forword_rev2
+//
+//  Created by Ibuki on 12/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Forword_rev2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
